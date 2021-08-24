825c675128db36b634faf0792f9801b0d4d4ace0
pipe1990 <afca100@hotmail.com>
2021-08-24 13:39:19 -0500

    hola, primer cambio

--- src/edu/unbosque/controller/Main.java
+++ src/edu/unbosque/controller/Main.java
@@ -3,7 +3,7 @@ package edu.unbosque.controller;
 public class Main {
 
 	public static void main(String[] args) {
-		String msg = "hola";
+		String msg = "hola mundo, como  estan??";
 		System.out.println(msg);
 	}
 
